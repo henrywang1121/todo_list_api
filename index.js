d86cf4dc5d0af53eb47baa8e4c9a5d5d55350c49
Henry Wang <henry.wang@live.com>
2019-04-14 20:01:06 +0000

    jQuery, AJAX, and styling

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 
 var todoRoutes = require('./routes/todos');
-
-app.get('/', function(req, res){
-   res.send('Hello from the root route');
-});
-
 app.use('/api/todos', todoRoutes);
 
+
+app.get('/', function(req, res){
+   res.sendFile('index.html');
+});
 
 app.listen(port, function(){
    console.log("Server Running: " + process.env.PORT); 
